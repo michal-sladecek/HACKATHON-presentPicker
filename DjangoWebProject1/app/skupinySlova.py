1c96fbaa0f01fc81693a460f62f506f94681d559
Prefix <michalsladecek98@gmail.com>
2016-04-29 16:43:45 +0200

    asdfds

--- DjangoWebProject1/app/skupinySlova.py
+++ DjangoWebProject1/app/skupinySlova.py
@@ -8,6 +8,6 @@ with open('parametre.pickle','rb') as f1:
     dic = pickle.load(f1)
     with open('skupiny.txt','r') as file:
         for line in file:
-            keywords[line] = dic
+            keywords[line.split()[0]] = dic
 with open('skupinySlova.pickle','wb+') as p:
     pickle.dump(keywords,p)
